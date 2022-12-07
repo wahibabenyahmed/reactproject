74c76bfff4e4c42eabbf1cb49a109de69ab2a741
wahibabenyahmed <wahibabenyahmed221@gmail.com>
2022-12-07 10:49:23 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavScrollExample from './navbar'
+import BasicExample from './form'
+import DefaultExample from './listgroupe'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavScrollExample/>
+      <BasicExample/>
+      <DefaultExample/>
     </div>
   );
 }
